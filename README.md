e00bc8ca11eda217963fe4a3e47c940af83edc29
weisong82 <19878334@qq.com>
2015-12-28 00:50:52 +0800

    更新了参考资料

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # requirejs-demo
 demo
+
+AMD和CMD的区别讨论
+http://div.io/topic/430
+https://www.zhihu.com/question/21347409#answer-2323656
+
+require sea 的一些讨论
+https://www.zhihu.com/question/20342350
+
+jquery 是AMD规范,sea js引用和插件包装
+seajs模块化jQuery与jQuery插件
+ http://julabs.com/blog/seajs-jquery-and-plugins/
--- sea/static/hello/hello.js
+++ sea/static/hello/hello.js
@@ -3,16 +3,20 @@ define(function(require, exports, module) {
 
     // 通过 require 引入依赖
     var $ = require('jQuery');
-    //var jqmd5 = require('jqmd5');
+    var jqmd5 = require('jqmd5');
 
     // 通过 exports 对外提供接口
     exports.title = function(title) {
             console.log("title called");
             alert("title");
         }
+    exports.m5 = function(m5){
+        console.log(jqmd5.abc())
+    }
     // 或者通过 module.exports 提供整个接口
     module.title2 = function(title) {
         console.log("title2 called");
         alert("title2");
     }
+
 });
